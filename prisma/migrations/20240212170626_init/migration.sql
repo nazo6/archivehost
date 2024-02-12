dd3cefd6702364f90792062933baf3888ddf1735
nazo6 <git@nazo6.dev>
2024-02-13 02:07:08 +0900

    Update migration

--- prisma/migrations/20240212170626_init/migration.sql
+++ prisma/migrations/20240212170626_init/migration.sql
@@ -1,12 +1,13 @@
 -- CreateTable
 CREATE TABLE "Archive" (
     "urlScheme" TEXT NOT NULL,
+    "urlHost" TEXT NOT NULL,
     "urlPath" TEXT NOT NULL,
     "timestamp" DATETIME NOT NULL,
     "mime" TEXT NOT NULL,
-    "status" TEXT,
+    "status" INTEGER,
     "savePath" TEXT NOT NULL
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Archive_urlScheme_urlPath_timestamp_key" ON "Archive"("urlScheme", "urlPath", "timestamp");
+CREATE UNIQUE INDEX "Archive_urlScheme_urlHost_urlPath_timestamp_key" ON "Archive"("urlScheme", "urlHost", "urlPath", "timestamp");
